d32050ca7036eedaf3f5e9096e36f1e67fb4d06d
dayvid <dayvid@192.168.0.110>
2019-01-12 00:42:43 -0300

    Iniciando Projeto Com Spring Boot

---
+++ src/main/java/com/infowaybr/infowaybank/InfowayBankApplication.java
@@ -0,0 +1,14 @@
+package com.infowaybr.infowaybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfowayBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfowayBankApplication.class, args);
+	}
+
+}
+
