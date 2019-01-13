9e777279659a6f7d96743641ab46407a42ce06f4
dayvid <dayvid@192.168.0.110>
2019-01-12 23:20:39 -0300

    Adicionando modelo do Usuário

--- src/main/java/com/infowaybr/infowaybank/models/User.java
+++ src/main/java/com/infowaybr/infowaybank/models/User.java
@@ -6,13 +6,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Bank {
+public class User {
 
 	@Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
 	private String name;
+
+	private String username;
+
+	private String password;
 
 	public Long getId() {
 		return id;
@@ -28,5 +32,21 @@ public class Bank {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
